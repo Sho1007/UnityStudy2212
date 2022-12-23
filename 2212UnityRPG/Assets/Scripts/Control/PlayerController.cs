0c48c69b938a147aab3af8b5fb43cdf125f60396
Sho1007 <sho1007@kookmin.ac.kr>
2022-12-23 23:52:06 +0900

    Add Combat Scripts / Edit PlayerController to Access to Combat Scripts

--- 2212UnityRPG/Assets/Scripts/Control/PlayerController.cs
+++ 2212UnityRPG/Assets/Scripts/Control/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
+using System;
 
 namespace RPG.Control
 {
@@ -16,21 +18,57 @@ namespace RPG.Control
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
+            print("Nothing to do.");
         }
 
-        private void MoveToCursor()
+        private static Ray GetMouseRay()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target != null)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        GetComponent<Fighter>().Attack(target);
+                        
+                    }
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private bool InteractWithMovement()
+        {
+            return MoveToCursor();
+        }
+
+        private bool MoveToCursor()
+        {
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
-                GetComponent<Mover>().MoveTo(hit.point);
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Mover>().MoveTo(hit.point);
+                }
+                return true;
             }
+
+            return false;
         }
     }
 }
