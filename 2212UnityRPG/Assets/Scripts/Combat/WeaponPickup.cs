59f44e3cc6eb42e794ea98582c78e28e225673f3
Sho1007 <sho1007@kookmin.ac.kr>
2023-02-10 15:33:35 +0900

    Add Respawing Pickup Function

--- 2212UnityRPG/Assets/Scripts/Combat/WeaponPickup.cs
+++ 2212UnityRPG/Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,30 @@ namespace RPG.Combat
 {
     public class WeaponPickup : MonoBehaviour
     {
+        [SerializeField] float respawnTime = 3.0f;
         [SerializeField] Weapon weapon = null;
         private void OnTriggerEnter(Collider other) {
             if (other.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                //Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            gameObject.GetComponent<Collider>().enabled = shouldShow;
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                transform.GetChild(i).gameObject.SetActive(shouldShow);
             }
         }
     }
